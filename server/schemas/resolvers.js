1b92972823ea8703dc634f398e07630402ea8dab
aksmagic31 <sammyzhang31@gmail.com>
2022-09-03 10:30:48 -0700

    chaged some resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,11 +5,8 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
-        );
-        return userData;
+      if (context.data) {
+          return User.findOne({ _id: context.data._id }).populate('savedBooks');
       }
       throw new AuthenticationError("Please Log In");
     },
@@ -22,23 +19,28 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("Invalid credentials");
+        throw new AuthenticationError("Not User found");
       }
 
       const correctPassword = await user.isCorrectPassword(password);
       if (!correctPassword) {
-        throw new AuthenticationError("Invalid credentials");
+        throw new AuthenticationError("Invalid Password");
       }
       const token = signToken(user);
 
       return { token, user };
     },
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, { username, email, password }, context) => {
+  
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+          return res.status(400).json({ message: 'Something is wrong!' });
+      }
       const token = signToken(user);
 
-      return { token, user };
-    },
+      return { token, user }
+  },
     saveBook: async (parent, { input }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
